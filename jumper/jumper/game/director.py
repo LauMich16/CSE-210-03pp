a2111429f909f233ca9853e90dd93d244fbdfdb1
Jarom Diaz <jarom3429@gmail.com>
2021-11-11 09:34:08 -0700

    Game can end now.

--- jumper/jumper/game/director.py
+++ jumper/jumper/game/director.py
@@ -81,6 +81,12 @@ class Director:
             self._puzzle.reveal_puzzle()
         else:
             self._jumper.cut_line()
+
+        if self._puzzle.is_solved():
+            self._keep_playing = False
+        
+        if self._puzzle.incorrect_guesses >= 4:
+            self._keep_playing = False
         
     def _do_outputs(self):
         """Outputs the important game information for each round of play. In 
