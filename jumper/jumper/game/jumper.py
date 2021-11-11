689586c2a668ff88c2ea16c64af099248bdc2b86
tapzola <93328588+tapzola@users.noreply.github.com>
2021-11-11 09:24:44 -0700

    Update jumper.py

--- jumper/jumper/game/jumper.py
+++ jumper/jumper/game/jumper.py
@@ -1,21 +1,71 @@
 class Jumper:
 
     def __init__(self):
-        self._lines = 0
+        self._lines = 4
 
     def draw_jumper(self):
         """Draws an image of the jumper based on the current number of lines"""
-        print("-------------")
-        print("")
-        print("/___/_\_\\")
-        print(" \\     /")
-        print("  \\   /")
-        print("   \O/")
-        print("    |")
-        print("   / \\")
-        print("")
-        print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+        if self._lines == 4:
+
+            print("-------------")
+            print("")
+            print("/___/_\_\\")
+            print(" \\     /")
+            print("  \\   /")
+            print("   \O/")
+            print("    |")
+            print("   / \\")
+            print("")
+            print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+        elif self._lines == 3:
+            print("-------------")
+            print("")
+            print(" \\     /")
+            print("  \\   /")
+            print("   \O/")
+            print("    |")
+            print("   / \\")
+            print("")
+            print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+        elif self._lines ==2:
+            print("-------------")
+            print("")
+            print("  \\   /")
+            print("   \O/")
+            print("    |")
+            print("   / \\")
+            print("")
+            print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+        elif self._lines == 1:
+
+            print("-------------")
+            print("")
+            print("   \O/")
+            print("    |")
+            print("   / \\")
+            print("")
+            print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+        else:
+            print("-------------")
+            print("")
+            print("   \\x/")
+            print("    |")
+            print("   / \\")
+            print("/\\/\\/\\/\\/\\/\\/\\/\\")
+
+
+
+
+
+    
 
     def cut_line(self):
         """Cuts a line from the jumper"""
         self._lines -= 1
+    
+
